ea03df00ee81c23892abc039cd8b4d83735dbd84
gravinliu <836276616@qq.com>
2018-07-18 10:56:42 +0800

    去除配置文件形式，改为绝对路径连接数据库

--- GTP-BulkRepositories/Repository/ImportRepository.cs
+++ GTP-BulkRepositories/Repository/ImportRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Dapper;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
-using Sino.Dapper;
-using Sino.Dapper.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace GTP_BulkRepositories
 {
-    public class ImportRepository : DapperRepositoryBase<Aspnetusers, string>, IImportRepository
+    public class ImportRepository : IImportRepository
     {
-        public ImportRepository(IDapperConfiguration configuration) : base(configuration, true)
+        public ImportRepository()
         {
-
+            
         }
 
         public async Task ImportExcel(Queue<DriverVM> listModel)
@@ -22,26 +21,19 @@ namespace GTP_BulkRepositories
             {
                 if (listModel != null && listModel.Count > 0)
                 {
-                    //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                    //MySqlConnection con = new MySqlConnection("server=192.168.1.208;userid=root;pwd=lyh888150;database=ccp(latest);sslmode=none;");
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                    MySqlConnection con = new MySqlConnection("server=192.168.1.208;userid=root;pwd=lyh888150;database=ccp(latest);sslmode=none;");
                     foreach (var item in listModel)
                     {
                         string sql = $"select * from aspnetusers where PhoneNumber={item.PhoneNumber}";
-                        var user = new Aspnetusers();
-                        using (ReadConnection)
-                        {
-                            user = await ReadConnection.QueryFirstOrDefaultAsync<Aspnetusers>(sql);
-
-                        }
+                        var user = await con.QueryFirstOrDefaultAsync<Aspnetusers>(sql);
                         if (user != null)
                         {
                             if (user.UserCategory != UserCategory.JFEmployees)
                             {
                                 user.UserCategory = UserCategory.JFEmployees;
-                                using (WriteConnection)
-                                {
-                                    await WriteConnection.UpdateAsync(user);
-                                }
+                                string sql1 = $"update aspnetusers set UserCategory={user.UserCategory} where Id={user.Id}";
+                                await con.ExecuteAsync(sql1);
                             }
                         }
                         else
@@ -63,10 +55,30 @@ namespace GTP_BulkRepositories
                                 Date = DateTime.Now,
                                 UserCategory = UserCategory.JFEmployees
                             };
-                            using (WriteConnection)
-                            {
-                                await WriteConnection.InsertAsync(newuser); ;
-                            }
+                            DynamicParameters param = new DynamicParameters();
+                            string sql2 = @"insert into aspnetusers(Id,PhoneNumber,UserName,CertificatePhotoCertificationState,DrivingLicenseKeyCertificationState,
+                                           IdentityCardCertificationState,LockoutEnabled,RealNameStatus,CertificationStatus,TrailerLicenseCertificationStatus,
+                                           VehiclePhotoCertificationStatus,RoleType,RoadTransportStatus,Date,UserCategory,Integral)
+                                           values(@Id,@PhoneNumber,@UserName,@CertificatePhotoCertificationState,@DrivingLicenseKeyCertificationState,
+                                           @IdentityCardCertificationState,@LockoutEnabled,@RealNameStatus,@CertificationStatus,@TrailerLicenseCertificationStatus,
+                                           @VehiclePhotoCertificationStatus,@RoleType,@RoadTransportStatus,@Date,@UserCategory,@Integral)";
+                            param.Add("@Id", newuser.Id);
+                            param.Add("@PhoneNumber", newuser.PhoneNumber);
+                            param.Add("@UserName", newuser.UserName);
+                            param.Add("@CertificatePhotoCertificationState", newuser.CertificatePhotoCertificationState);
+                            param.Add("@DrivingLicenseKeyCertificationState", newuser.DrivingLicenseKeyCertificationState);
+                            param.Add("@IdentityCardCertificationState", newuser.IdentityCardCertificationState);
+                            param.Add("@LockoutEnabled", newuser.LockoutEnabled);
+                            param.Add("@RealNameStatus", newuser.RealNameStatus);
+                            param.Add("@CertificationStatus", newuser.CertificationStatus);
+                            param.Add("@TrailerLicenseCertificationStatus", newuser.TrailerLicenseCertificationStatus);
+                            param.Add("@VehiclePhotoCertificationStatus", newuser.VehiclePhotoCertificationStatus);
+                            param.Add("@RoleType", newuser.RoleType);
+                            param.Add("@RoadTransportStatus", newuser.RoadTransportStatus);
+                            param.Add("@Date", newuser.Date);
+                            param.Add("@UserCategory", newuser.UserCategory);
+                            param.Add("@Integral", newuser.Integral);
+                            await con.ExecuteAsync(sql2);
                         }
                     }
                 }
